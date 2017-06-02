968db8fb85a93ca0d99cfc69fdb47d661727d98e
Emiliotelo <edtlopez01@gmail.com>
2017-06-02 19:42:59 +0200

    Meta google añadida

--- iaw/buscador/urls.py
+++ iaw/buscador/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from django.contrib.auth.decorators import login_required, permission_required
 from django.conf.urls.static import static
 from buscador.views import *
+from django.conf.urls.static import static
 
 urlpatterns = [
         url('',include('social.apps.django_app.urls', namespace='social')),
@@ -10,6 +11,8 @@ urlpatterns = [
 		url(r'^articulo/del/(?P<pk>[0-9]+)/$',login_required(permission_required(ArticuloDelete.as_view(), 'articulo_del', raise_exception=True),login_url="/login/"),name="artdel"),
 		url('^', include('django.contrib.auth.urls')),
         url(r'^$', Articulolist.as_view(),name='home'),
+
+
          
   		
 
